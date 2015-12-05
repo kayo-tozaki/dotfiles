ee7b2604876db1bbf39501d584bcda26997a033a
kayo_tozaki <nec_git@naccent.dip.jp>
2015-12-05 16:25:10 +0900

    neobundle installer

--- installer.sh
+++ installer.sh
@@ -46,7 +46,8 @@ chmod +x ~/dotfiles/prezto.sh
 zsh ~/dotfiles/prezto.sh
 
 # NeoBundle 
-
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 }
 
 main
--- prezto.sh
+++ prezto.sh
@@ -1,13 +1,13 @@
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 setopt EXTENDED_GLOB
 
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do 
+   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}" 
+done
+
 cp ~/dotfiles/.zpreztorc ~/.zprezto/runcoms/
 cp ~/dotfiles/.zprofile ~/.zprezto/runcoms/
 cp ~/dotfiles/.zshenv ~/.zprezto/runcoms/
 cp ~/dotfiles/.zshrc ~/.zprezto/runcoms/
-
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do 
-   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}" 
-done
 
 chsh -s /bin/zsh 
