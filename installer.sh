4d7b5fa407807a3fa2e8f8098135edd295257009
kayo_tozaki <nec_git@naccent.dip.jp>
2015-12-05 15:15:11 +0900

    add installer.sh
    Test Edition for CentOS

--- installer.sh
+++ installer.sh
@@ -1,9 +1,48 @@
-OS=`uname -a`
+function ubuntu () {
+        sudo apt-get -y install git vim zsh
+}
 
-case "$OS" in
- *"Ubuntu"* ) echo "Ubuntu";;
- *"Linux"* ) echo "Linux"  ;; 
- *"Darwin"* ) echo "Darwin(MacOS)" ;;
- * ) echo "none" ;;
-esac
+function Linux () {
+ Dist=`cat /proc/version`
+ case "$Dist" in
+  *"Red Hat"* ) RedHat ;;
+  * ) echo "none Linux OS" ;;
+ esac
+}
+
+function RedHat () {
+ case "`cat /etc/redhat-release`" in
+  *"CentOS"* ) echo "CentOS" ;;
+  * ) echo "none RedHat OS" ;;
+ esac
+}
+
+function CentOS () {
+  sudo yum install -y git zsh vim
+}
+
+function main () {
+
+cd ~
+
+# judge Distribution
+ OS=`uname -a`
+ case "$OS" in
+  *"Ubuntu"* ) ubuntu ;;
+  *"Linux"* ) Linux  ;;
+  *"Darwin"* ) echo "Darwin(MacOS)" ;;
+  * ) echo "none" ;;
+ esac
+ 
+# clone dotfiles
+git clone https://github.com/kayo-tozaki/dotfiles.git ~/dotfiles
+cp -Rf ~/dotfiles/.* ~/
+
+# prezto
+zsh ~/dotfiles/prezto.sh
+
+}
+/
+main
+
 
